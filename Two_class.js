698205e2858449e6f32c8ac5a92e7f90ba564ece
Luiz <luiz_f_maria@estudante.sesisenai.org.br>
2024-03-25 08:35:38 -0300

    enemy mov

--- Two_class.js
+++ Two_class.js
@@ -26,3 +26,35 @@ class Player extends Obj{
     }
 }
 
+class Inimigo extends Obj{
+    vida_inimigo = 5
+    pararY = 180  // posição onde o inimigo vai parar
+    tempoProximoTiro = Math.random() * 100 // tempo até o próximo tiro do inimigo
+    intervaloTiro = 60 // intervalo de tempo entre os tiros dos inimigos
+    tempoTiro = 0
+
+    atual_inimigo(){
+        if(this.y <= this.pararY){
+            this.y += 0.5
+        }else if(this.vida_inimigo <= 0){
+            this.recomeca()
+        }
+
+        this.tempoTiro++
+        if(this.tempoTiro >= this.tempoProximoTiro){
+            this.tempoTiro = 0
+            this.atira()
+            this.tempoProximoTiro = Math.random() * 350 // define um novo tempo para o próximo tiro
+        }
+    }
+
+    vel = Math.random() * (2 - 0.5) + 0.5
+
+    mov(){
+        if(this.y <= this.pararY){
+            this.y += 0.5
+        }
+
+    }
+
+}
