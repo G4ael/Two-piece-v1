fbe2095b24f8b04871f05e8d1445c91df3211c05
Luiz <luiz_f_maria@estudante.sesisenai.org.br>
2024-03-25 08:35:15 -0300

    player mov

--- Two_class.js
+++ Two_class.js
@@ -1,3 +1,28 @@
 class Text extends Obj{
-    
+
 }
+
+class Player extends Obj{
+
+    mov(){
+        this.speed += this.dir * 0.1
+        this.speed *= 0.9; // Aplicando uma pequena resistência para suavizar o movimento
+        this.x += this.speed
+
+        // Limitando a velocidade máxima do jogador
+        if (this.speed > 3) {
+            this.speed = 3
+        } else if (this.speed < -3) {
+            this.speed = -3
+        }
+
+        if(this.x <= 0){
+            this.x = 0
+            this.speed = 0
+        } else if(this.x >= 900){
+            this.x = 900
+            this.speed = 0
+        }
+    }
+}
+
